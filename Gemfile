d44cda587a64f99ee41e3992e8619635b0b78a86
Макс <m.chichev@rocketguys.com>
2020-04-01 21:49:20 +0300

    fuck 500, lets sentry them

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'mastercard_currencyconversion'
 gem 'bootstrap-sass', '3.4.1'
+gem 'sentry-raven'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Lite
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    Raven.configure do |config|
+      config.dsn = 'https://6c7754fe0a03455e8273d457e60382f5:204ae7e49664417da639be9de26f6f24@sentry.io/4829428'
+      config.environments = %w[production]
+      config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
